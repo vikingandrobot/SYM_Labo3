797d010734ce662297ec2cdaf748f923b12dbcd5
Michela Zucca <michela.zucca@heig-vd.ch>
2017-12-08 19:10:25 +0100

    Implémentation de code barre QR code et beacon

--- app/src/main/java/ch/heigvd/iict/sym/labo3/CodesBarresActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym/labo3/CodesBarresActivity.java
@@ -1,13 +1,23 @@
 package ch.heigvd.iict.sym.labo3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class CodesBarresActivity extends AppCompatActivity {
+    private IntentIntegrator integrator;
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,38 @@ public class CodesBarresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_codes_barres);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        this.button = findViewById(R.id.button_scann);
+
+        this.button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               scanning();
+            }
+        });
+
+        this.integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan a QR code");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(true);
+
     }
 
+    public void scanning(){
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
--- app/src/main/java/ch/heigvd/iict/sym/labo3/IBeaconActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym/labo3/IBeaconActivity.java
@@ -1,20 +1,241 @@
 package ch.heigvd.iict.sym.labo3;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.RemoteException;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class IBeaconActivity extends AppCompatActivity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class IBeaconActivity extends AppCompatActivity implements BeaconConsumer {
+    protected static final String TAG = "**** MonitoringActivity";
+    private BeaconManager beaconManager;
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    // GUI
+    private ListView list = null;
+    private BeaconAdapter adapter = null;
+    private TextView empty = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ibeacon);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        this.list = findViewById(R.id.list);
+        // this.empty = findViewById(R.id.list_empty);
+
+        this.adapter = new BeaconAdapter(this);
+        this.list.setAdapter(adapter);
+        //  this.list.setEmptyView(empty);
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.bind(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons in the background.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                    @TargetApi(23)
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+
+                });
+                builder.show();
+            }
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+    }
+
+
+    private void verifyBluetooth() {
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
+            }
+        } catch (RuntimeException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                    System.exit(0);
+                }
+
+            });
+            builder.show();
+
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    adapter.addBeacon(beacons);
+                    int i = 0;
+                    for (Beacon beacon : beacons) {
+                        Log.i(TAG, "--- The beacon [" + i++ + "] " + beacon.toString() + " is about " + beacon.getDistance() + " meters away.");
+                    }
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private class BeaconAdapter extends BaseAdapter {
+        private List<Beacon> beaconsList = null;
+        private Context context = null;
+
+        public BeaconAdapter(Context context) {
+            this.context = context;
+            this.beaconsList = new ArrayList<>();
+        }
+
+        public void addBeacon(Collection<Beacon> beacon) {
+            this.beaconsList.clear();
+            this.beaconsList.addAll(beacon);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    notifyDataSetChanged();
+                }
+            });
+            Log.i(TAG, "addBEACON ON LIST");
+        }
+
+        @Override
+        public int getCount() {
+            return beaconsList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return beaconsList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View recycledView, ViewGroup viewGroup) {
+            if (recycledView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                recycledView = inflater.inflate(R.layout.list_beacon_view, viewGroup, false);
+            }
+
+            //link to GUI
+            TextView UUID =  recycledView.findViewById(R.id.list_item_UUID);
+            TextView RSSI =  recycledView.findViewById(R.id.list_item_RSSI);
+            TextView min =  recycledView.findViewById(R.id.list_item_Min);
+            TextView max =  recycledView.findViewById(R.id.list_item_Max);
+            TextView name = recycledView.findViewById(R.id.list_item_Name);
+
+            Beacon beacon = (Beacon) getItem(position);
+
+            UUID.setText("UUID : " + beacon.getId1());
+            RSSI.setText("RSSI : " + beacon.getRssi());
+            min.setText("Min : " + beacon.getId2().toString());
+            max.setText("Max : " + beacon.getId3().toString());
+            name.setText(""+ beacon.getParserIdentifier().toUpperCase());
+
+            return recycledView;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+    }
 }
